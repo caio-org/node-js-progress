a215f6b2237ac68ecbb027d31fc43e1b7b40f978
Caio Amaral <caiobr98@gmail.com>
2023-07-31 11:41:07 -0300

    fix

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const index = require('./index')
 test('Aplicar desconto', () => {
     const result = index.aplicarDesconto(10,5);
-    expect(result).toEqual(5);
+    expect(result).toEqual(4);
 })
